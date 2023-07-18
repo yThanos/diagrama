3765c042033ea5ebbead9db5652a8a29b2a5e244
Vitor Fraporti <vitorfraporti@hotmail.com>
2023-07-18 12:05:32 -0300

    s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,19 @@ export class AppComponent {
   @ViewChild(DxDiagramComponent, { static: false }) diagram!: DxDiagramComponent;
 
   constructor(http: HttpClient) {
-    http.get('./data.json').subscribe((data) => {
+    http.get('./data.json').subscribe((data) => {//não faz nada literamente da erro, mas funciona
       this.diagram.instance.import(JSON.stringify(data));
+      this.teste();
     }, (err) => {
       throw 'Data Loading Error';
     });
+  }
+
+  teste(){
+    while(true){
+      setTimeout(() => {
+        console.log(this.diagram)
+      },5000)
+    }
   }
 }
